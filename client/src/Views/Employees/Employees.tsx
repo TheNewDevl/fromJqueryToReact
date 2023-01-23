22981c576469de37e696fc08f7e4952aef2a73ba
carlos <carl.devfs@gmail.com>
2023-01-23 15:23:39 +0100

    client: integrate react table package

--- client/src/Views/Employees/Employees.tsx
+++ client/src/Views/Employees/Employees.tsx
@@ -1,9 +1,14 @@
 import style from "./Employees.module.scss";
+import { ReactTable } from "data-table-react";
 
 interface EmployeesProps {}
 
 export const Employees = ({}: EmployeesProps) => {
-  return <div className={style.Employees}>Employees List</div>;
+  return (
+    <div className={style.Employees}>
+      <ReactTable />
+    </div>
+  );
 };
 
 /** Created by carlos on 13/01/2023 */
