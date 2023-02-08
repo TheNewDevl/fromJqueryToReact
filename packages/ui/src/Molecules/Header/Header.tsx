d2184aaf241216214be234fee437ac857520dac0
carlos <carl.devfs@gmail.com>
2023-02-08 13:23:40 +0100

    ui: simplify header and remove image to be closer to the initial project

--- packages/ui/src/Molecules/Header/Header.tsx
+++ packages/ui/src/Molecules/Header/Header.tsx
@@ -1,22 +1,15 @@
 import style from "./Header.module.scss";
-import logo from "../../assets/logohrnet.jpeg";
-import { Image } from "../../Atoms/Image/Image";
 import { Title } from "../../Atoms/Title/Title";
 import { Link } from "react-router-dom";
 
-interface HeaderProps {}
-
-export const Header = ({}: HeaderProps) => {
+export const Header = () => {
   return (
     <header className={style.Header}>
-      <div className={style.logoContainer}>
-        <Link to={"/"}>
-          <Image src={logo} alt={"hrnet logo"} />
-        </Link>
-      </div>
-      <Title level={"1"}>
-        <span className={style.coloredSpan}>HT</span>net
-      </Title>
+      <Link to={"/"}>
+        <Title level={"1"}>
+          <span className={style.coloredSpan}>HT</span>net
+        </Title>
+      </Link>
     </header>
   );
 };
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,5 +1,4 @@
 export * as globalStyles from "./styles/globalStyles.scss";
-export { Image } from "./Atoms/Image/Image";
 export { Title } from "./Atoms/Title/Title";
 export { Header } from "./Molecules/Header/Header";
 export { Link } from "./Atoms/Link/Link";
