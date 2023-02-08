ac8611072867de9400abe2c08046acf997012833
carlos <carl.devfs@gmail.com>
2023-02-08 13:25:11 +0100

    clint: update Employees.tsx

--- client/src/Views/Employees/Employees.tsx
+++ client/src/Views/Employees/Employees.tsx
@@ -1,16 +1,27 @@
 import style from "./Employees.module.scss";
-import { ReactTable } from "data-table-react";
+import { DataTable, TableColumns } from "@carlco/data-table-react";
 import { useContext } from "react";
 import { EmployeesContext } from "../../context/EmployeesContext";
+import { mockEmployees } from "../../utils/mocks";
 
-interface EmployeesProps {}
+const columns: TableColumns = [
+  { title: "First Name", data: "firstName" },
+  { title: "Last Name", data: "lastName" },
+  { title: "Start Date", data: "startDate", type: "date" },
+  { title: "Department", data: "department" },
+  { title: "Date of Birth", data: "dateOfBirth", type: "date" },
+  { title: "Street", data: "street" },
+  { title: "City", data: "city" },
+  { title: "State", data: "state" },
+  { title: "Zip Code", data: "zipCode" },
+];
 
-export const Employees = ({}: EmployeesProps) => {
+export const Employees = () => {
   const { employees } = useContext(EmployeesContext);
 
   return (
     <div className={style.Employees}>
-      <ReactTable />
+      <DataTable data={employees.length > 0 ? employees : mockEmployees} columns={columns} />
     </div>
   );
 };
